12ea7cbbf0a5d12681fb3f51ca312abfa5ba40b8
Roy <roylayindeay0@gmail.com>
2022-09-22 06:55:19 +0100

    example project sorted

---
+++ src/modules/students/models/getStudents.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface IStudent{
+    userId: string
+    name: string
+    email: string
+    points: number
+}
+
+interface IGetStudentsDocument extends Document {
+    students: Array<IStudent>,
+    noOfStudents: number
+}
+
+const GetStudentsSchema = new Schema<IGetStudentsDocument>(
+    {
+        noOfStudents: Number,
+        students: [{
+            _id: false,
+            userId: String,
+            name: String,
+            email: String,
+            points: Number
+        } ]
+    },
+    {
+        timestamps: true,
+    }
+)
+
+export default mongoose.model<IGetStudentsDocument>('AllStudents', GetStudentsSchema)
---
+++ src/modules/students/student.route.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import eventHandler from '../app/eventHandler'
+import studentController from './student.controller'
+import studentEventHandler from './student.events'
+
+const router = express.Router()
+const studentEvent = new studentEventHandler();
+
+//GET
+router.get('/', studentController.getStudents)
+router.get('/student/:id', studentController.getStudent)
+
+//POST
+router.post('/student', studentController.addStudent)
+router.post('/student/:id/take-attendance', studentController.takeAttendance)
+
+//DELETE
+router.delete('/student/:id', studentController.removeStudent)
+
+//ADMIN-ENDPOINTS-THAT-RECOMPUTE
+router.post('/student/:id/re-compute', (req, res) => {
+    eventHandler.attendanceHandler({userId: req.params.id});
+    res.json({
+        message: 'User data recomputed',
+    })
+});
+router.post('/re-compute', async (req, res) => {
+    eventHandler.studentHandler({}, false);
+    res.json({
+        message: 'User data recomputed',
+    })
+});
+
+export default router
